0706ea136da1431a67e37ca29b70e88fe96e4920
John Kyrus <john@biggestmini.lan>
2020-06-13 15:00:20 -0400

    localhost

--- healthcheck.py
+++ healthcheck.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 
-import os
 import sys
 import urllib.request
 
-print(urllib.request.urlopen(os.environ.get('HEALTHCHECK_URL')).getcode())
+print(urllib.request.urlopen('https://localhost:8889/roku/health').getcode())
 
-if urllib.request.urlopen(os.environ.get('HEALTHCHECK_URL')).getcode() == 200:
+if urllib.request.urlopen('https://localhost:8889/roku/health').getcode() == 200:
     sys.exit(0)
 else:
     sys.exit(1)
